00808ff9607741ece5f44d9aad596c065f5a1fe9
Samizie <samilincoln@hotmail.com>
2024-01-01 23:49:42 -0800

    following standard

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Module - script that starts a Flask web application"""
+""" A script that starts a flask web application """
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/',strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Handles the root url"""
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
-
-if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
